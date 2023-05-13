40647f1b916943757949d1c27b490c66ae72ddb4
ThousandAITech <thousand.ai.tech@gmail.com>
2023-05-13 23:42:57 +0800

    1. admin Custom MyUser Model

--- ltserLyudao/user/admin.py
+++ ltserLyudao/user/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import MyUser
+class MyUserAdmin(admin.ModelAdmin):
+    list_display = ('get_email', 'get_name', 'get_verified', 'get_last_login')
 
-# Register your models here.
+    def get_email(self, obj):
+        return obj.email
+
+    def get_verified(self, obj):
+        return obj.is_verified
+
+    def get_name(self, obj):
+        return obj.first_name+obj.last_name
+
+    def get_last_login(self, obj):
+        return obj.last_login
+
+    get_email.short_description = 'EMAIL ADDRESS'
+    get_verified.short_description = 'VERIFIED STATUS'
+    get_name.short_description = 'NAME'
+    get_last_login.short_description = 'LAST LOGIN'
+
+admin.site.register(MyUser, MyUserAdmin)
