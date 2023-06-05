301c3377e0bb36536254713dd7a8ed47e3236496
ThousandAITech <thousand.ai.tech@gmail.com>
2023-06-05 23:36:23 +0800

    1. MEDIA_ROOT, MEDIA_URL

--- ltserLyudao/ltserLyudao/urls.py
+++ ltserLyudao/ltserLyudao/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -22,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('user.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
