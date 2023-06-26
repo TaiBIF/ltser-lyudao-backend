7da9f49f6889c4e650bded40fc3a7b8a5e93b42d
ThousandAITech <thousand.ai.tech@gmail.com>
2023-06-26 16:44:04 +0800

    api/data/plant/detail/ done

--- ltserLyudao/api/urls.py
+++ ltserLyudao/api/urls.py
@@ -4,7 +4,7 @@ from .views import GetWeatherTimeRangeView, GetWeatherDetailView, GetSeaTemperat
 
 urlpatterns = [
     path('data/time-range/', GetWeatherTimeRangeView.as_view(), name='get_weather_time_range'),
-    path('data/detail/', GetWeatherDetailView.as_view(), name='get_weather_detail'),
+    path('data/weather/detail/', GetWeatherDetailView.as_view(), name='get_weather_detail'),
     path('data/sea-temperature/time-range/', GetSeaTemperatureTimeRangeView.as_view(),
          name='get_sea_temperature_time_range'),
     path('data/sea-temperature/detail/', GetSeaTemperatureDetailView.as_view(), name='get_sea_temperature_detail'),
