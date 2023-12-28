1ae429e3a0d3d3c5db216da459ecdad9a15ead8e
ThousandAITech <thousand.ai.tech@gmail.com>
2023-12-28 15:27:06 +0900

    [update] is_regular_user

--- ltserLyudao/user/models.py
+++ ltserLyudao/user/models.py
@@ -32,6 +32,7 @@ class MyUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     is_verified = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    is_regular_user = models.BooleanField(default=True, null=True, blank=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     first_name = models.CharField(max_length=255)
