ed8a9dd974bf07940680d892439fad75b40eee99
ThousandAITech <thousand.ai.tech@gmail.com>
2024-01-15 23:24:33 +0800

    [update] docker deploy

--- ltserLyudao/entrypoint.sh
+++ ltserLyudao/entrypoint.sh
@@ -5,4 +5,4 @@
 python manage.py makemigrations
 python manage.py migrate
 
-exec gunicorn ltserLyudao.wsgi:application --bind 0.0.0.0:8200
+exec gunicorn ltserLyudao.wsgi:application --bind 0.0.0.0:8000
