c31a060a044a06fe0571a3a9da3e28a06a76c1d7
MooGoo <moogoo78@gmail.com>
2024-01-18 22:53:35 +0800

    fix: set static files, put in docker volumes

--- ltserLyudao/entrypoint.sh
+++ ltserLyudao/entrypoint.sh
@@ -12,5 +12,3 @@ python manage.py collectstatic --no-input
 #gunicorn ltserLyudao.wsgi:application --bind 0.0.0.0:8000
 
 python manage.py runserver 0.0.0.0:8000
-
-fi
--- ltserLyudao/ltserLyudao/settings.py
+++ ltserLyudao/ltserLyudao/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(',')
 
-ALLOWED_HOSTS = ['*']
 #ALLOWED_HOSTS = ['127.0.0.1', 'www.thousandaitech.com', 'thousandaitech.com']
 
 AUTH_USER_MODEL = 'user.MyUser'
