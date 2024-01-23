75802ef9cc4d7d85765b334161777d5ac6f34aef
ThousandAITech <thousand.ai.tech@gmail.com>
2024-01-23 18:23:48 +0800

    [update] admin.py coraldata

--- ltserLyudao/api/admin.py
+++ ltserLyudao/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CoralData
 
-# Register your models here.
+class CoralDataAdmin(admin.ModelAdmin):
+    list_display = ('dataID', 'eventID', 'year', 'month', 'day', 'time', 'locationID', 'verbatimLocality', 'locality', 'verbatimDepth', 'decimalLatitude', 'decimalLongitude', 'replicate', 'scientificName', 'taxonRank', 'family', 'identificationRemarks', 'measurementType', 'measurementValue', 'measurementUnit', 'individualCount', 'recordedBy', 'identifiedBy', 'samplingProtocol', 'sampleSizeValue', 'sampleSizeUnit')
+    search_fields = ['dataID', 'eventID', 'locality', 'scientificName']
+    # 更多自定義設置...
+
+admin.site.register(CoralData, CoralDataAdmin)
