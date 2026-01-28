9b8782d6ade2d5c0244d03dbeff233758f8dbedc
JJJHANG <jangzhu5678@gmail.com>
2026-01-28 15:15:04 +0800

    update: email recipients

--- ltserLyudao/api/utils/email_recipients.py
+++ ltserLyudao/api/utils/email_recipients.py
@@ -2,7 +2,10 @@ DEFAULT_TO = ["ltsertwlyudao@gmail.com"]
 DEFAULT_CC = ["ltsertwlyudao@gmail.com"]
 
 OBSERVATION_EMAILS = {
-    "海域水質": {"to": ["huckyuan@gmail.com"], "cc": ["likefh@gmail.com"]},
+    "海域水質": {
+        "to": ["huckyuan@gmail.com"],
+        "cc": ["likefh@gmail.com", "kunchang.li@gmail.com"],
+    },
     "陸域植物": {
         "to": ["galanhsnu@gmail.com"],
         "cc": ["bochung@gate.sinica.edu.tw", "kunchang.li@gmail.com"],
